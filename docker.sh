58f13a1e52c103b32f248227ca6672473b5b5e75
Roler <roler.huaman@gmail.com>
2023-06-22 16:03:34 -0500

    dato2

--- docker.sh
+++ docker.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-mkdir remigio1000
 cd remigio1000
-git init
-git pull https://github.com/rolhuama/pruebasdecodigo.git
 gradle bootjar
 docker build -t remigio1000 .
-docker tag remigio1000 rolervengador/remigio1000
 docker push rolervengador/remigio1000
-docker run -dp 8080:8080 remigio1000
